715831258d713ebb4e28588295c789d2764a268d
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-27 10:47:10 -0500

    sweetalert added to add lesson

--- client/scripts/controllers/LessonController.js
+++ client/scripts/controllers/LessonController.js
@@ -4,5 +4,17 @@ myApp.controller('LessonController', ['$scope', '$http', '$location', 'UserServi
   console.log('in LessonController');
   $scope.addLesson = function() {
     console.log('add lesson function running');
+    sweetAlert();
   };
+
+// pop-up success alert once lesson has been added
+  sweetAlert = function() {
+    swal({
+    title: 'Success!',
+    text: 'Sweet, your lesson has been added!',
+    type: 'success',
+  });
+    $location.path('/user');
+};
+
 }]);
