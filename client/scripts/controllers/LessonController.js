9cb067b2e6c0ca84b1cb4c721b2476fc4351b506
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-07 17:19:58 -0500

    lesson edit updates, style updates, code cleanup

--- client/scripts/controllers/LessonController.js
+++ client/scripts/controllers/LessonController.js
@@ -1,4 +1,4 @@
-myApp.controller('LessonController', ['$scope', '$http', '$location', 'UserService', 'StudentService', 'MailService', function($scope, $http, $location, UserService, StudentService, MailService) {
+myApp.controller('LessonController', ['$scope', '$http', '$location', 'UserService', 'StudentService', 'MailService', 'AlertService', function($scope, $http, $location, UserService, StudentService, MailService, AlertService) {
 
   $scope.getStudents = StudentService.getStudents;
   $scope.getStudents();
@@ -13,83 +13,80 @@ myApp.controller('LessonController', ['$scope', '$http', '$location', 'UserServi
   $scope.sendEmail = MailService.sendEmail;
   $scope.lessonObject = StudentService.lessonObject;
   $scope.viewDetails = StudentService.viewDetails;
-
+  $scope.sweetAlertModal = AlertService.sweetAlertModal;
+  //object for form to bind to.
+  $scope.lesson = {};
   $scope.editing = {status: 'FALSE'};
+
   $scope.changeEditing = function() {
     $scope.editing.status = 'TRUE';
+    $scope.lesson.id = $scope.lessonObject.data.id;
+    $scope.lesson.assigned = 'FALSE';
+    $scope.lesson.date = $scope.lessonObject.data.date;
+    $scope.lesson.email = $scope.lessonObject.data.email;
+    $scope.lesson.student = $scope.lessonObject.data.student;
   };
 
   console.log('logging userObject: ', $scope.userObject);
   console.log('logging student object: ', $scope.studentObject);
-  //object for form to bind to.
 
-  $scope.lesson = {};
 
+  //Deletes lesson from database, initiates location move and modal
   $scope.deleteLesson = function(lesson) {
     //DELETE to delete lesson from database
     console.log('in deleteLesson');
     $http.delete('/lesson/' + lesson.id).then(function(response){
       console.log('back from the server with success!', response);
     });
-    swal({
-      title: 'No prob, bob!',
-      text: 'Just like that, your lesson has been deleted.',
-      type: 'success',
-    });
+    $scope.sweetAlertModal('No prob, bob!', 'Just like that, your lesson has been deleted.');
     $location.path('/user');
   };
 
   //Creates Lesson when form is submitted on addlesson.html form.
-  // POST Route to SERVER
   $scope.saveLesson = function(lesson) {
     $scope.lesson = lesson;
-    // $scope.lesson.date = moment(lesson.date).format("MMM Do, YYYY");
     //reformats date before it's sent to the database
     lesson.date = moment(lesson.date).calendar('days');
-    console.log('logging scope.lesson: ', $scope.lesson);
-    console.log('logging lesson before it goes to DB: ', lesson);
-
-    //POST saves lesson to database
     $http.post('/lesson', lesson).then(function(response){
-      console.log('back from the server with success!', response);
       $location.path('/user');
-      //sweetAlert creates success modal
-      sweetAlert();
+      // sweetAlert creates success modal
+      $scope.sweetAlertModal('Success!', 'Sweet, your lesson has been added!', 'success');
     });
     //function that sends email to student with lesson information
     // $scope.sendEmail(lesson);
-  }; //ends addLesson function
+  }; //ends saveLesson function
 
   $scope.getLessons();
 
-  // pop-up success alert once lesson has been added
-  sweetAlert = function() {
-    swal({
-      title: 'Success!',
-      text: 'Sweet, your lesson has been added!',
-      type: 'success',
-    });
-    $location.path('/user');
-  };//ends saveLesson
-
-  //
+  //Updates lesson in database to be 'assigned', initiates sending of email
   $scope.assignLesson = function(lesson) {
-    //POST saves lesson to database
-    console.log('logging lesson.id in assignLesson: ', lesson.id);
     $scope.sendEmail(lesson);
-
-    $http.put('/lesson/' + lesson.id).then(function(response){
+    $http.put('/lesson/assign/' + lesson.id).then(function(response){
       console.log('back from the server with success!', response);
       $location.path('/user');
-      //sweetAlert creates success modal
-      swal({
-        title: 'Success!',
-        text: 'Sweet, your lesson has been emailed!',
-        type: 'success',
-      });
+      $scope.sweetAlertModal('Success!', 'Sweet, your lesson has been emailed!');
     });
     // sends email to student
     // $scope.sendEmail(lesson);
+  };//ends assignLesson
+
+
+//Updates lesson details from 'updatelesson' edit mode
+  $scope.updateLesson = function(lesson) {
+    //POST saves lesson to database
+    console.log('logging lesson.id in assignLesson: ', lesson.id);
+
+    // $http.put('/lesson/' + lesson.id).then(function(response){
+    //   console.log('back from the server with success!', response);
+    //   $location.path('/user');
+    //   //sweetAlert creates success modal
+    //   swal({
+    //     title: 'Success!',
+    //     text: 'Sweet, your lesson details have been updated!',
+    //     type: 'success',
+    //   });
+    // });
+
   };//ends assignLesson
 
 }]);
--- client/scripts/factories/AlertService.js
+++ client/scripts/factories/AlertService.js
@@ -1,8 +1,15 @@
 myApp.factory('AlertService', ['$http', '$location', function($http, $location){
 
+function sweetAlertModal(customTitle, customText) {
+  swal({
+    title: customTitle,
+    text: customText,
+    type: 'success',
+  });
+}
 
 return {
-
+  sweetAlertModal: sweetAlertModal
 };
 
 }]);
